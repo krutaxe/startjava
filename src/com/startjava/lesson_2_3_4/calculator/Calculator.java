3a12a58cb134c66aa12e9789315ce48b273b702f
krutaxe <krutaxe@mail.ru>
2024-09-10 13:49:26 +0300

    implement Lesson_4_HW_Task2-fix4

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,11 +1,17 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
     public double calculate(String[] expression) {
-
-        double num2 = Integer.parseInt(expression[2]);
-        final double num1 = Integer.parseInt(expression[0]);
+        int num1 = Integer.parseInt(expression[0]);
+        int num2 = Integer.parseInt(expression[2]);
         char operation = expression[1].charAt(0);
+
+        if ((operation == '/' || operation == '%') && num2 == 0) {
+            return Double.NaN;
+        }
+
         double result = 0;
         switch (operation) {
             case '+':
@@ -15,17 +21,40 @@ public class Calculator {
             case '*':
                 return num1 * num2;
             case '/':
-                if (num2 == 0) {
-                    return Double.NaN;
-                }
-                return num1 / num2;
+                return (double) num1 / num2;
             case '%':
-                return Math.floorMod((int) num1, (int) num2);
+                return Math.floorMod(num1, num2);
             case '^':
                 return Math.pow(num1, num2);
             default:
                 System.out.println("Нераспознанная операция");
         }
         return result;
+    }
+
+    public boolean isValidExpression(String[] expression, Scanner scanner) {
+        if (expression.length != 3) {
+            System.out.println("Выражение введено не корректно, попробуйте ещё раз.");
+            return false;
+        }
+
+        while (!expression[0].matches("[-+]?\\d+")) {
+            System.out.print("Первое значение не является числом, введите ещё раз: ");
+            expression[0] = scanner.nextLine();
+        }
+
+        while (!(expression[1].charAt(0) == '+' || expression[1].charAt(0) == '-' || expression[1].charAt(0) == '*' ||
+                expression[1].charAt(0) == '/' || expression[1].charAt(0) == '^' || expression[1].charAt(0) == '%')) {
+            System.out.println("Ошибка: операция " + expression[1].charAt(0) + " не поддерживается.");
+            System.out.println("Доступны следующие операции: +, -, *, /, ^, %");
+            System.out.print("Введите знак операции (+, -, *, /, %, ^)");
+            expression[1] = scanner.nextLine();
+        }
+
+        while (!expression[2].matches("[-+]?\\d+")) {
+            System.out.print("Второе значение не является числом, введите ещё раз: ");
+            expression[2] = scanner.nextLine();
+        }
+        return true;
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -4,28 +4,21 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CalculatorTest {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String answer = "yes";
-
+        Calculator calculator = new Calculator();
         while (answer.equals("yes")) {
             System.out.print("Введите математическое выражение(например 2 + 5): ");
             String[] expression = scanner.nextLine().split(" ");
-            if (expression.length != 3) {
-                System.out.println("Выражение введено не корректно, попробуйте ещё раз.");
-                continue;
-            }
+            if (!calculator.isValidExpression(expression, scanner)) continue;
 
-            Calculator calculator = new Calculator();
-            DecimalFormat dc;
             double result = calculator.calculate(expression);
-            if (result % 1 == 0) {
-                dc = new DecimalFormat("#");
+            if (Double.isNaN(result)) {
+                System.out.println("Результат: Not-a-Number");
             } else {
-                dc = new DecimalFormat("#.###");
+                printResult(expression, result);
             }
-            System.out.println(expression[0] + " " + expression[1] + " " + expression[2] + " = " + dc.format(result));
 
             do {
                 System.out.print("Хотите продолжить вычисления? [yes/no]: ");
@@ -33,5 +26,10 @@ public class CalculatorTest {
             } while (!answer.equals("no") && !answer.equals("yes"));
         }
         System.out.println("Завершение работы калькулятора.");
+    }
+
+    public static void printResult(String[] expression, double result) {
+        DecimalFormat df = new DecimalFormat("#.###");
+        System.out.println(expression[0] + " " + expression[1] + " " + expression[2] + " = " + df.format(result));
     }
 }
