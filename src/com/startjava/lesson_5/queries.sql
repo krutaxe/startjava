b752d5aaa717689ed78312f55fc6f70ee0c9da8d
krutaxe <krutaxe@mail.ru>
2024-10-01 14:26:16 +0300

    implementation Lesson_5_HW_Task1-fix2

--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,55 +1,55 @@
 \echo Вся таблица
 select *
-from jaegers
-order by model_name;
+  from jaegers
+ order by model_name;
 
 \echo Только не уничтоженные роботы
 select *
-from jaegers
-where status != 'Destroyed'
-order by model_name;
+  from jaegers
+ where status != 'Destroyed'
+ order by model_name;
 
 \echo Только роботы серии Mark-1 и Mark-4
 select *
-from jaegers
-where mark in ('Mark-1', 'Mark-4');
+  from jaegers
+ where mark
+    in ('Mark-1', 'Mark-4');
 
 \echo Всех роботов, кроме Mark-1 и Mark-4, отсортировав таблицу по убыванию по столбцу mark
 select *
-from jaegers
-where mark not in ('Mark-1', 'Mark-4')
-order by mark desc;
+  from jaegers
+ where mark
+not in ('Mark-1', 'Mark-4')
+ order by mark desc;
 
 \echo Информация о самых старых роботах
 select *
-from jaegers
-where launch = (select min(launch)
-                from jaegers);
+  from jaegers
+ where launch = (select min(launch) from jaegers);
 
 \echo Информация из столбцов model_name, mark, launch, kaiju_kill тех роботов, которые уничтожили больше всех kaiju
 select model_name, mark, launch, kaiju_kill
-from jaegers
-where kaiju_kill = (select max(kaiju_kill)
-                    from jaegers);
+  from jaegers
+ where kaiju_kill = (select max(kaiju_kill) from jaegers);
 
 \echo Средний вес роботов, округлив его до трех знаков после запятой
 select round(avg(weight), 3) as avg_weight
-from jaegers;
+  from jaegers;
 
 \echo Увеличить на единицу количество уничтоженных kaiju у неразрушенных роботов, а затем отобразить таблицу
 with updated as (
     update jaegers
-        set kaiju_kill = kaiju_kill + 1
-        where status = 'Alive'
-        returning *)
+       set kaiju_kill = kaiju_kill + 1
+     where status = 'Alive'
+    returning *)
 select *
-from updated;
+  from updated;
 
 \echo Удалить уничтоженных роботов, а затем отобразить оставшихся
 with deleted as (
     delete from jaegers
-        where status = 'Destroyed'
-        returning *)
+          where status = 'Destroyed'
+    returning *)
 select *
-from jaegers
-where status not in ('Destroyed');
+  from jaegers
+ where status not in ('Destroyed');
